7564e190866120c05603eee833b4dda7acfa5baf
sobin98 <71113430+sobin98@users.noreply.github.com>
2022-08-12 01:47:43 +0900

    Add files via upload

---
+++ lib/shuttle.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Shuttle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('셔틀')),
+
+    );
+  }
+}
